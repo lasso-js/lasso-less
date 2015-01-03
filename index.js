58dadb2890391d4628c6fbb24b96a91c3a49abd0
Patrick Steele-Idem <pnidem@gmail.com>
2015-01-02 17:05:27 -0700

    De-promisified code from PR

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // native node modules
 var nodePath = require('path');
-
+var fs = require('fs');
 
 // public modules from npm
-var logger = require('raptor-logging').logger(module),
-    resolveFrom = require('resolve-from'),
-    Q = require('q'),
-    FS = require('q-io/fs');
+var async = require('async');
+var logger = require('raptor-logging').logger(module);
+var resolveFrom = require('resolve-from');
+var requireRegExp = /^require\s*:\s*(.+)$/; // Hack: {2,} is used because Windows file system paths start with "c:\"
 
-var lessPath = null,
-    less;
+
+var lessPath = null;
+var less;
 
 try {
     lessPath = require.resolve('less');
@@ -21,184 +22,153 @@ try {
 
 var lessOptimizer = function(optimizer, config) {
 
-
-    var mixins = {
+    optimizer.dependencies.registerStyleSheetType('less', {
         properties: {
             path: 'string',
             paths: 'string',
             imports: 'object'
         },
-    };
 
-    mixins.init = function(optimizerContext, callback) {
+        init: function(optimizerContext, callback) {
 
-        if (!this.path) {
-            return callback(new Error('"path" is required for a less dependency'));
-        }
+            if (!this.path) {
+                return callback(new Error('"path" is required for a less dependency'));
+            }
 
-        if (!less) {
-            return callback(new Error('Unable to handle Less dependency for path "' + this.path + '". The "less" module was not found. This module should be installed as a top-level application module.'));
-        }
+            if (!less) {
+                return callback(new Error('Unable to handle Less dependency for path "' + this.path + '". The "less" module was not found. This module should be installed as a top-level application module.'));
+            }
 
-        this.path = this.resolvePath(this.path);
-        callback();
-    };
+            this.path = this.resolvePath(this.path);
+            callback();
+        },
 
-    mixins.loadDependencies = function(){
+        read: function(optimizerContext, callback) {
+            var _this = this;
+            var path = this.path;
+            var paths = [nodePath.dirname(path)];
+            var imports = this.imports;
+            var completed = false;
 
-        var REQUIRE_PATTERN = 'require:';
-            
-        var deferred = Q.defer(),
-            promises = [],
-            lessCode = '';
+            if (require.main && require.main.paths) {
+                paths = paths.concat(require.main.paths);
+            }
 
-        //if imports doesn't exist return null.
-        if (!this.imports || !this.imports.length) {
-            deferred.resolve(lessCode);
-            return deferred.promise;
-        }
+            if (config.paths) {
+                paths = config.paths.concat(paths);
+            }
 
-        // loop through all the imports and create array of promises to read the files.
-        this.imports.forEach(function(importPath, index){
-            var pattern = new RegExp('^' + REQUIRE_PATTERN),
-                splittedImport,
-                promise;
+            function errorCallback(err){
+                logger.error(err);
 
-            importPath = importPath.trim();
+                if (completed) {
+                    return;
+                }
+                completed = true;
+                callback(err);
+            }
 
-            if(pattern.exec(importPath)){
-                splittedImport = importPath.split(REQUIRE_PATTERN);
-
-                // no path found after require:
-                if( splittedImport.length !== 2){
+            function renderCallback(err, output) {
+                if (err) {
+                    if (err.line !== null) {
+                        err = new Error('Error compiling Less file (' +
+                            err.filename +
+                            ':' +
+                            err.line +
+                            ':' +
+                            err.column +
+                            ') - ' +
+                            err.message);
+                    }
+                    errorCallback(err);
                     return;
                 }
 
-                importPath = splittedImport[1].trim();
-            }
-
-            // get full path
-            importPath = resolveFrom(this.getParentManifestDir(), importPath);
-            
-            promise = FS.read(importPath, {
-                flags: 'r',
-                charset: 'utf-8'
-            });
-            
-            promises.push(promise);
-
-            
-        }.bind(this));
-
-        if(promises.length){
-            // read all the files in order and concat the results.
-            Q.all(promises)
-                .spread(function(){
-                    var args = [].slice.call(arguments);
-                    lessCode = args.join('\n');
-
-                    // return concatinated less code
-                    deferred.resolve(lessCode);
-                })
-                .fail(function(error){
-                    deferred.reject(error);
-                });
-
-        }else{
-            deferred.resolve(null);
-        }
-
-        return deferred.promise;
-    
-    }; // < /  mixins.loadDependencies >
-
-
-    mixins.read = function(optimizerContext, callback) {
-
-        var path = this.path,
-            paths = [
-                nodePath.dirname(path)
-            ],
-            depLessCode = '';
-
-        if (require.main && require.main.paths) {
-            paths = paths.concat(require.main.paths);
-        }
-
-        if (config.paths) {
-            paths = config.paths.concat(paths);
-        }
-
-        var errorCallback = function(err){
-            logger.error(err);
-            callback(err);
-        };
-
-        var renderCallback = function(err, output) {
-            if (err) {
-                if (err.line !== null) {
-                    err = new Error('Error compiling Less file (' +
-                                    err.filename + 
-                                    ':' + 
-                                    err.line + 
-                                    ':' + 
-                                    err.column + 
-                                    ') - ' +
-                                    err.message);
+                if (completed) {
+                    return;
                 }
-                errorCallback(err);
-                return;
+
+                completed = true;
+
+                logger.info('Finished parsing Less file at path: ' + path);
+
+                // LESS v2+ returns an object with "css" property.
+                // Old versions returned just the CSS.
+                callback(null, (output.css === null) ? output : output.css);
             }
 
-            logger.info('Finished parsing Less file at path: ' + path);
+            function readImports(callback){
+                //if imports doesn't exist return null.
+                if (!imports || !imports.length) {
+                    return callback(null, '');
+                }
 
-            // LESS v2+ returns an object with "css" property.
-            // Old versions returned just the CSS.
-            callback(null, (output.css === null) ? output : output.css);
-        };
+                async.map(
+                    imports,
+                    function(importPath, callback) {
+                        importPath = importPath.trim();
 
-        var readFileCallback = function(lessCode) {
+                        var requireMatches = requireRegExp.exec(importPath);
+                        var isRequire = false;
 
-            lessCode = depLessCode.concat('\n', lessCode);
+                        if (requireMatches){
+                            isRequire = true;
+                            importPath = requireMatches[1].trim();
+                            importPath = resolveFrom(_this.getParentManifestDir(), importPath);
+                        } else {
+                            importPath = _this.resolvePath(importPath);
+                        }
 
-            var parseConfig = {
-                filename: path,
-                paths: paths
-            };
-            
-            logger.info('Parsing Less file at path: ' + path);
+                        fs.readFile(importPath, 'utf8', callback);
+                    },
+                    function(err, results) {
+                        if (err) {
+                            return callback(err);
+                        }
+                        callback(null, results.join('\n'));
+                    });
+            }
 
-            less.render(lessCode, parseConfig, renderCallback);
-        };
+            function readFile(callback) {
+                fs.readFile(path, {encoding: 'utf8'}, callback);
+            }
 
-        var dependencyCallback = function(lessCode){
-            
-            depLessCode = lessCode;
+            async.parallel(
+                [
+                    readImports,
+                    readFile
+                ],
+                function(err, results) {
+                    if (err) {
+                        return callback(err);
+                    }
 
-            //read main file
-            FS.read(path, {
-                flags: 'r',
-                charset: 'utf-8'
-            })
-            .then(readFileCallback)
-            .fail(errorCallback);
-        };
+                    var importedCode = results[0];
+                    var lessCode = results[1];
 
-        this.loadDependencies()
-            .then(dependencyCallback.bind(this))
-            .fail(errorCallback);
+                    if (importedCode) {
+                        lessCode = importedCode + '\n' + lessCode;
+                    }
 
-    }; // < /  mixins.load >
+                    var parseConfig = {
+                        filename: path,
+                        paths: paths
+                    };
 
-    mixins.getSourceFile = function() {
+                    logger.info('Parsing Less file at path: ' + path);
+
+                    less.render(lessCode, parseConfig, renderCallback);
+                });
+        }, // < /  load >
+
+        getSourceFile: function() {
             return this.path;
-    };
+        },
 
-    mixins.getLastModified = function(optimizerContext, callback) {
+        getLastModified: function(optimizerContext, callback) {
             return callback(null, -1);
-    };
-    
-    optimizer.dependencies.registerStyleSheetType('less', mixins);
+        }
+    });
 };
 
 module.exports = lessOptimizer;
